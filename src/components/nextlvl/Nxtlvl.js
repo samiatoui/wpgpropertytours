530edfadfa074944e8b7bad64de795b533a44742
Sami <atoui79@gmail.com>
2022-05-15 18:46:53 -0500

    website

--- src/components/nextlvl/Nxtlvl.js
+++ src/components/nextlvl/Nxtlvl.js
@@ -3,16 +3,29 @@ import "./nxtlvl.css"
 export default function Contact() {
     return (
         <div className="nxtlvl">
-            <div className="title-nxtlvl">
-
-                <h1>Take your listings to the next level</h1>
+            <div>
+                <div className="title-nxtlvl">
+                    <h1>Why do you need Virtual Tours?</h1>
+                </div>
+                <div className="text-nxtlvl">
+                    The data indicates that Virtual Tours are becoming essential for every listing. Here are a few interesting statistics:
+                </div>
+                <div className="img-nxtlvl">
+                    <img src="https://i.imgur.com/54BR2c0.png"></img>
+                </div>
             </div>
-            <div className="text-nxtlvl">
-                Listings that utilize 360 Virtual Tours have been proven to perform better.
-            </div>
-            <div className="img-nxtlvl">             
-                <img src="https://i.imgur.com/54BR2c0.png"></img>
+            <div className="why">
+                <div className="tile">
+                    Realtor.com statistics report that listings with virtual tours receive <mark>87%</mark> more views than home listings without tours and that <mark>54%</mark> of buyers skip over listings of properties that don’t include virtual tours.
+                </div>
+                <div className="tile">
+                    <mark>50%</mark> of adult users on the internet rely on virtual tours in their research and decision-making process.
+                </div>
+                <div className="tile">
+                    According to a Redfin survey of more than 1,900 home buyers across 32 major real estate markets, <mark>63%</mark> made an offer on a property that they hadn’t seen in person. That number is twice the number in 2021 than in 2020.
+                </div>
             </div>
         </div>
+
     )
 }
