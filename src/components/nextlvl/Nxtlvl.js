412c65b3cacb67b9664c6786f92aeff6b33aaa63
Sami <atoui79@gmail.com>
2022-05-16 00:53:20 -0500

    test

--- src/components/nextlvl/Nxtlvl.js
+++ src/components/nextlvl/Nxtlvl.js
@@ -17,7 +17,6 @@ export default function Contact() {
             <div className="why">
                 <div className="tile">
                     Listings with virtual tours receive <mark>87%</mark> more views than home listings without tours
-
                 </div>
                 <div className="tile">
                     <mark>54%</mark> of buyers skip over listings of properties that don’t include virtual tours.
