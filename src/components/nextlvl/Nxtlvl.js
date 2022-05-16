7899a9da52d7c12867130411d0960b15ea0a66b5
Sami <atoui79@gmail.com>
2022-05-16 00:49:28 -0500

    test

--- src/components/nextlvl/Nxtlvl.js
+++ src/components/nextlvl/Nxtlvl.js
@@ -8,7 +8,7 @@ export default function Contact() {
                     <h1>Why do you need Virtual Tours?</h1>
                 </div>
                 <div className="text-nxtlvl">
-                    The data indicates that Virtual Tours are becoming essential for every listing. Here are a few interesting statistics:
+                    The data indicates that Virtual Tours are becoming essential for every listing
                 </div>
                 <div className="img-nxtlvl">
                     <img src="https://i.imgur.com/54BR2c0.png"></img>
@@ -16,13 +16,14 @@ export default function Contact() {
             </div>
             <div className="why">
                 <div className="tile">
-                    Realtor.com statistics report that listings with virtual tours receive <mark>87%</mark> more views than home listings without tours and that <mark>54%</mark> of buyers skip over listings of properties that don’t include virtual tours.
+                    Listings with virtual tours receive <mark>87%</mark> more views than home listings without tours
+
                 </div>
                 <div className="tile">
-                    <mark>50%</mark> of adult users on the internet rely on virtual tours in their research and decision-making process.
+                    <mark>54%</mark> of buyers skip over listings of properties that don’t include virtual tours.
                 </div>
                 <div className="tile">
-                    According to a Redfin survey of more than 1,900 home buyers across 32 major real estate markets, <mark>63%</mark> made an offer on a property that they hadn’t seen in person. That number is twice the number in 2021 than in 2020.
+                    <mark>63%</mark> made an offer on a property that they hadn’t seen in person.
                 </div>
             </div>
         </div>
