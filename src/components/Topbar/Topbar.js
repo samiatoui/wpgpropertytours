898ef14ec721dee21f8ff1432feddea8ccaef539
Sami Atoui <atoui79@gmail.com>
2022-08-22 13:12:33 -0500

    test

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,6 +1,5 @@
 import "./topbar.css";
 import React, { Component } from "react";
-import MenuItems from "./MenuItems";
 
 class Topbar extends Component {
     render() {
@@ -9,13 +8,6 @@ class Topbar extends Component {
                 <div className="title">
                     <img src="https://i.imgur.com/WClBKEW.png"></img>
                 </div>
-                <ul>
-                    {MenuItems.map((item, index) => {
-                        return (
-                            <li><a className="item.cName" href={item.url}>{item.title}</a></li>
-                        )
-                    })}
-                </ul>
             </nav>
         )
     }
