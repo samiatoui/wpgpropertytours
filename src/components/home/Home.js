7c44287b98b2cb7d527eebf8373744fdc8dc70f7
Sami Atoui <atoui79@gmail.com>
2022-12-13 12:26:24 -0600

    changes done

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,16 +1,19 @@
 import "./home.css"
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 export default function About() {
+
+
     return (
 
         <div className="about">
             <div className="text">
-            <img src="https://i.imgur.com/vqDQYja.png"></img>
+                <img src="https://i.imgur.com/vqDQYja.png"></img>
                 <p>Providing virtual tour services for all types of properties.</p>
 
-                <button className="contactme" onClick='window.location="#contact"'>
-                    Get in touch.
-                </button>
+                    <a href="https://https://www.instagram.com/wpgpropertytours/" className="contactme">
+                        Instagram
+                    </a>
             </div>
             <div className="imghome">
                 <img src="https://i.imgur.com/F7vIDAY.png"></img>
